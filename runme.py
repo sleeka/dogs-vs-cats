5d7ac72c91f47ce046d49ddda6af4f03e567de35
Sean Leeka <sleeka@swbell.net>
2017-05-15 00:52:54 -0500

    Keras 2 API introduced several function and parameter names Convolutional2D --> Conv2D fit_generator parameter name changes

--- runme.py
+++ runme.py
@@ -6,7 +6,7 @@
 # import keras	# Machine Learning
 from keras.models import Sequential
 from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 import numpy as np 	# Mathematics
@@ -35,15 +35,15 @@ train_generator = datagen.flow_from_directory(
 
 # Step 2 - Build Model
 model = Sequential()
-model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height, 3)))
+model.add(Conv2D(32, 3, 3, input_shape=(img_width, img_height, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Convolution2D(32,3,3))
+model.add(Conv2D(32,3,3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Convolution2D(64,3,3))
+model.add(Conv2D(64,3,3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
@@ -62,13 +62,13 @@ model.compile(loss='binary_crossentropy',
 model.fit_generator(
 	train_generator,
 		# Multiplied by ten for bigger dataset (25k images)
-	samples_per_epoch=20480,
+	steps_per_epoch=20480,
 		# Multiplied by ten for bigger dataset (25k images)
-	nb_epoch=30,
+	epoch=30,
 	# validation_data=validation_data
 	validation_data=train_generator,
 		# Multiplied by ten for bigger dataset (25k images)
-	nb_val_samples=8320)
+	validation_steps=8320)
 model.save_weights('models/simple_CNN.h5')
 
 # Step 4 - Test Model
