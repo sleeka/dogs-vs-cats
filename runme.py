5afcd6a3e0d5f6bebea8949370b3ed52776f63e3
Sean Leeka <sleeka@swbell.net>
2017-05-15 00:34:43 -0500

    -->Cleaned libraries -->Sorted images into train/dogs and train/cats with the following command:     find . -name 'dogs*' -exec mv -t ./dogs {}+ -->Had trouble installing scipy within virtualenv.  Tried Anaconda, which installed globally.     Finally symlinked a scipy dir in virtualenv site-packages to /usr/lib/.../scipy

--- runme.py
+++ runme.py
@@ -3,21 +3,44 @@
 # Kaggle data, dogs vs cats
 # Taught by Siraj Raval
 
-import keras	# Machine Learning
+# import keras	# Machine Learning
+from keras.models import Sequential
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
 import numpy as np 	# Mathematics
-from parses import load_data 	# data loading
+
+# cannot import name load_data
+# from parser import load_data 	# data loading
+
+# Image dimensions
+img_width, img_height = 150, 150
 
 # Step 1 - Collect Data
-training_data = load_data('data/training')
-validation_data = load_data('data/validation')
+train_data_dir = 'train'
+# used to rescale the pixel values from [0, 255] to [0, 1] interval
+datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = datagen.flow_from_directory(
+	train_data_dir,
+	target_size=(img_width, img_height),
+	batch_size=16,
+	class_mode='binary')
+
+# cannot import name load_data
+# training_data = load_data('train')
+# below I'll try using the same set for validation
+# validation_data = load_data('train')
 
 # Step 2 - Build Model
 model = Sequential()
-model.add(Convolution2D(32,3,3 input_shape=(img_width, img_height, 3)))
+model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Convolution2D(32,3,3))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Convolution2D(64,3,3))
@@ -26,6 +49,7 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())
 model.add(Dense(64))
+model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
@@ -36,15 +60,19 @@ model.compile(loss='binary_crossentropy',
 
 # Step 3 - Train Model
 model.fit_generator(
-	training_data,
-	samples_per_epoch=2048,
-	nb_epoch=2048,
-	validation_data=validation_data
-	nb_val_samples=832)
+	train_generator,
+		# Multiplied by ten for bigger dataset (25k images)
+	samples_per_epoch=20480,
+		# Multiplied by ten for bigger dataset (25k images)
+	nb_epoch=30,
+	# validation_data=validation_data
+	validation_data=train_generator,
+		# Multiplied by ten for bigger dataset (25k images)
+	nb_val_samples=8320)
 model.save_weights('models/simple_CNN.h5')
 
 # Step 4 - Test Model
-img = image.load_img('test/img001.jpg', target_size=(224,224))
+img = image.load_img('test/1.jpg', target_size=(224,224))
 prediction = model.predict(img)
 print prediction
 
